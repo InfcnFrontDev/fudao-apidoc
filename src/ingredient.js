88dc5e98f62c2d83291cea2ae2937e5b434b4f8e
杨可可 <yangkk@infcn.com.cn>
2017-04-11 15:39:42 +0800

    update

--- src/ingredient.js
+++ src/ingredient.js
@@ -1,43 +1,47 @@
 /**
- * @api {get} /IngredientApi/getIngredientList 获取食材信息
+ * @api {get} /IngredientApi/getIngredient 获取食材详细
  * @apiVersion 2.0.0
- * @apiName getIngredientList
+ * @apiName getIngredient
  * @apiGroup Ingredient
  * @apiPermission login
  *
- * @apiDescription 获取食材的详细信息。
+ * @apiDescription 根据食材名称，获取食材的详细。
  *
  * @apiHeader {String} access-key Users unique access-key.
  *
  * @apiParam {String} name 		食材名称
  *
- * @apiSuccess {Boolean}    ok               是否成功
- * @apiSuccess {String}     name             食材名称
- * @apiSuccess {String}     otherName        其他的名字
- * @apiSuccess {String}     img              图片
- * @apiSuccess {String}     abstract         摘要
- * @apiSuccess {String}     protein          蛋白质
- * @apiSuccess {String}     fat              脂肪
- * @apiSuccess {String}     carbohydrate     碳水化合物
- * @apiSuccess {String}     dietaryFiber     膳食纤维
- * @apiSuccess {String}     cholesterol      胆固醇
-
-
+ * @apiSuccess {Boolean}    ok                   是否成功
+ * @apiSuccess {Object}     obj                  食材对象
+ * @apiSuccess {String}     obj.id               ID
+ * @apiSuccess {String}     obj.name             名称
+ * @apiSuccess {String}     obj.otherName        其他的名字
+ * @apiSuccess {String}     obj.img              图片
+ * @apiSuccess {String}     obj.abstract         摘要
+ * @apiSuccess {String}     obj.protein          蛋白质
+ * @apiSuccess {String}     obj.fat              脂肪
+ * @apiSuccess {String}     obj.carbohydrate     碳水化合物
+ * @apiSuccess {String}     obj.dietaryFiber     膳食纤维
+ * @apiSuccess {String}     obj.cholesterol      胆固醇
+ *
  *
  * @apiSuccessExample {json} Success-Response:
  *     HTTP/1.1 200 OK
  *     {
  *         "ok": true,
-*         "name": "蚕豆",
-*         "otherName": "大豆",
-*         "img": "ingredien/1.1.jpg",
-*         "abstract":"蚕豆有。。的作用。。",
-*         "protein": "5.0",
-*         "fat": "0.7",
- *         "carbohydrate": "0.8",
- *         "dietaryFiber": "0.9",
- *         "cholesterol": "0.6"
- *      }
+ *         "obj": {
+ *             "id": "11081c38-0d7d-11e7-9b59-000c293e6828",
+ *             "name": "蚕豆",
+ *             "otherName": "大豆",
+ *             "img": "ingredien/1.1.jpg",
+ *             "abstract":"蚕豆有。。的作用。。",
+ *             "protein": "5.0",
+ *             "fat": "0.7",
+ *             "carbohydrate": "0.8",
+ *             "dietaryFiber": "0.9",
+ *             "cholesterol": "0.6"
+ *         }
+ *     }
  *
  */
-function getMyIngredientList() { return; }
+function getIngredient() { return; }
